45ccc0aa2f3e6fcd7be3e44d9d043929eb82cd75
ClassroomAccount <jpepe44@gmail.com>
2020-04-30 20:10:35 -0400

    add intent for dialer

--- app/src/main/java/com/course/example/customadapter/MainActivity.java
+++ app/src/main/java/com/course/example/customadapter/MainActivity.java
@@ -1,5 +1,7 @@
 package com.course.example.customadapter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -22,18 +24,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         listView.setOnItemClickListener(this);
 
         //create ArrayList values
-        arrayList.add(new MyData(1, " Allison","987-576-4435"));
-        arrayList.add(new MyData(2, " Robert","878-757-6768"));
-        arrayList.add(new MyData(3, " Jason","657-576-5765"));
+        arrayList.add(new MyData(1, " Allison","9875764435"));
+        arrayList.add(new MyData(2, " Robert","8787576768"));
+        arrayList.add(new MyData(3, " Jason","6575765765"));
+        arrayList.add(new MyData(4, " Mary","6595765999"));
 
         //create custom adapter and connect to ListView
         adapter = new MyAdapter(this, arrayList);
         listView.setAdapter(adapter);
     }
 
+    //create toast and open dialer for the person selected
     public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
         String name = arrayList.get(position).getName();
         Toast.makeText(this, "Hello, " + name, Toast.LENGTH_LONG).show();
+        String phone = arrayList.get(position).getMobileNumber();
+        Uri uri = Uri.parse("tel:" + phone);
+        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(intent);
     }
 
 }
